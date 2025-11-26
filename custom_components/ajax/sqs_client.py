92993bbeab2bf5d3cb4940fe66a077301ef196d4
Stéphane <foxace66@gmail.com>
2025-11-26 07:14:26 +0100

    Fix lint: use collections.abc for Callable

--- custom_components/ajax/sqs_client.py
+++ custom_components/ajax/sqs_client.py
@@ -7,7 +7,8 @@ import json
 import logging
 import threading
 import time
-from typing import Any, Callable
+from collections.abc import Callable
+from typing import Any
 
 try:
     from aiobotocore.session import get_session
